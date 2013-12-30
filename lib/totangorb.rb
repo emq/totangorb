6c703ccd6cae3ca85bca175b32a3c53f52819c25
Michał Darda <michaldarda@gmail.com>
2013-12-30 15:04:31 +0100

    Hacks for Ruby 1.9

--- lib/totangorb.rb
+++ lib/totangorb.rb
@@ -1,3 +1,7 @@
+require_relative '../ext/nil_class'
+require_relative '../ext/open_struct'
+require_relative '../ext/hash'
+
 require "totangorb/version"
 require 'totangorb/tracker'
 require 'totangorb/errors'
